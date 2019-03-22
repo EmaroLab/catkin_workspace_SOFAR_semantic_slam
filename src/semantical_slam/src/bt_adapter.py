eef8f3713d7db96ed4311c1c34cf4c3ed30d58b2
GiovanniBorre <giovaborr@gmail.com>
2019-03-22 12:09:26 +0100

    add not working wait

---
+++ src/semantical_slam/src/bt_adapter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import string
+
+
+rospy.init_node('bt_adapter')
+
+
+def callback(msg_from_adapter, args):
+	actual_place=string()
+	actual_place=loromsg.namedtupl
+	pub.publsh(actual_place)
+	
+
+	
+#main
+def main():
+	sub=rospy.Subscriber('topic su cui pubblica bt ', string, callback,)
+	pub=rospy.Publisher('adapted_message', string)
+	rospy.spin()
+
+if __name__== "__main__":
+  main()
