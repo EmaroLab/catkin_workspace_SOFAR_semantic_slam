a05d00719ad3913c6621392fe6649545e93fb660
GiovanniBorre <giovaborr@gmail.com>
2019-03-29 16:24:52 +0100

    better launch

--- launch.sh
+++ launch.sh
@@ -3,17 +3,17 @@
 #sourcing roslaunch orb_slam2_ros orb_slam2_mono.launch
 source devel/setup.bash
 
-echo -n "Type 'create' in order to create a new map or 'load' in order to load an existing map.
-" 
-read MODE
+echo -n "Type 'create' in order to create a new map or 'load' in order to load an existing map." 
 echo
+read MODE
 case "$MODE" in
 create)
-	echo -n "___________________Creation mode___________________
-"
-	read -p "This will delete any previos map, type y to continue
-" -n 1 -r
-	echo    # (optional) move to a new line
+	echo -n "___________________Creation mode___________________"
+	echo
+	echo
+	echo "This will delete any previos map, type y to continue."
+	read REPLY
+	echo    
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
    		rm -rf src/orb_slam2_ros/ros/Bagfile
@@ -26,14 +26,14 @@ create)
 	;;
 
 load)	
-	echo -n "___________________Load mode___________________
-"
+	echo -n "___________________Load mode___________________"
+	echo
 	#terminator -T "orb_slam" -e "roslaunch orb_slam2_ros orb_slam2_mono.launch" &
 	#terminator -T "semantical_slam" -e "roslaunch semantical_slam semantical_slam.launch" &
 	;;
 *)
- 	echo -n "Unknown mode
-"
+ 	echo -n "Unknown mode."
+	echo
 	;;
 
 esac
