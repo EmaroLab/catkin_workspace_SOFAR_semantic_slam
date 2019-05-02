effeade015abd40cbe8f3f3bc7cf3d7dfe69b21b
GiovanniBorre <giovaborr@gmail.com>
2019-05-02 21:08:45 +0200

    Aggiunto il controllo della partenza sull adapter

--- src/semantical_slam/src/tf_to_pose_adapter.py
+++ src/semantical_slam/src/tf_to_pose_adapter.py
@@ -20,14 +20,15 @@ def main():
 	pub=rospy.Publisher('/adapted_pose', Pose, queue_size=10)
 	
 	rate = rospy.Rate(10.0)
-	while not rospy.is_shutdown():
-        	try:	
-			now = rospy.Time.now()
-			listener.waitForTransform('map', 'camera_link', now, rospy.Duration(4.0))
-            		adapted_pose_stamped = listener.transformPose('map', miro_pose_stamp)
-			pub.publish(adapted_pose_stamped.pose)
-        	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        		continue
+	if listener.frameExist('map') and listener.frameExist('camera_link'):
+		while not rospy.is_shutdown():
+			try:	
+				now = rospy.Time.now()
+				listener.waitForTransform('map', 'camera_link', now, rospy.Duration(4.0))
+		    		adapted_pose_stamped = listener.transformPose('map', miro_pose_stamp)
+				pub.publish(adapted_pose_stamped.pose)
+			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+				continue
 	rate.sleep()
 
 if __name__== "__main__":
