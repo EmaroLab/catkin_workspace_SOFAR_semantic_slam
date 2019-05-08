275d10846940ec7ebac4a87b7e2f52918061ce26
tommi95 <tommygruppi@gmail.com>
2019-05-08 14:02:16 +0200

    9 may

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/tommi/catkin_workspace_SOFAR_semantic_slam/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/tommi/mdk2/devel;/opt/ros/kinetic".split(';') if "/home/tommi/mdk2/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- src/semantical_slam/src/tf_to_pose_adapter.py
+++ src/semantical_slam/src/tf_to_pose_adapter.py
@@ -4,7 +4,9 @@ import math
 
 #import msgs type
 from std_msgs.msg import String
-import tf 
+import tf
+#import tf2_ros
+#import tf2_geometry_msgs
 from geometry_msgs.msg import PoseStamped, Pose	
 
 #main
@@ -16,18 +18,28 @@ def main():
 	
 	miro_pose_stamp.pose.position=[0,0,0]
 	miro_pose_stamp.pose.orientation=[0,0,0,1]
+
+	rospy.loginfo("#############################################################")
+	rospy.loginfo(miro_pose_stamp)
 	
 	listener = tf.TransformListener()
+	#print("We are here:")
+	#print(listener)
+	#tfBuffer = tf2_ros.Buffer()
+	#listener = tf2_ros.TransformListener(tfBuffer)
 
 	pub=rospy.Publisher('/adapted_pose', Pose, queue_size=10)
 	
 	rate = rospy.Rate(10.0)
 	while not rospy.is_shutdown():
+		#print (listener.frameExists('map'))
 		if listener.frameExists('map') and listener.frameExists('camera_link'):
 			try:	
 				now = rospy.Time.now()
-				listener.waitForTransform('map', 'camera_link', now, rospy.Duration(4.0))
+				#listener.waitForTransform('map', 'camera_link', now, rospy.Duration(4.0))
 		    		adapted_pose_stamped = listener.transformPose('map', miro_pose_stamp)
+				#print("We are here 2:")
+				#print(adapted_pose_stamped)
 				adapted_pose=adapted_pose_stamped.pose
 				pub.publish(adapted_pose)
 			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
