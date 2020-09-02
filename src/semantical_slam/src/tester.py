661e945b3070f4c5357047a13deb76f00a8cfbe0
Tommaso Gruppi <44870996+tommi95@users.noreply.github.com>
2020-09-02 19:15:08 +0200

    Update tester.py

--- src/semantical_slam/src/tester.py
+++ src/semantical_slam/src/tester.py
@@ -1,6 +1,7 @@
-#This script is meant for testing the nodes. 
-#Some random string are published in the '/adapted_message' topic and then the process is suspended.
 #!/usr/bin/env python
+
+#This script is meant for testing the nodes. Some random string are published in the '/adapted_message' topic and then the process is suspended.
+
 import rospy
 import random 
 
@@ -13,20 +14,20 @@ from semantical_slam.msg import SemanticalPoint
 from semantical_slam.srv import CreateSemMsg
 
 def test():
-	#publisher and node are initialized
+	#Initialization of the publisher to the topic named '/adapted_message' and the node named 'test'
 	pub = rospy.Publisher('/adapted_message', String, queue_size=10)
 	rospy.init_node('test')
-	#frequecy of published msg is set
+	#Set the frequency of the published msg
 	rate = rospy.Rate(0.1)
-	#we simulate to publish a semantic information (string type) hypotetically acquired from beacons
+	#Possible simulated messages to be published as semantic information (string type) hypotetically acquired from beacons or object recognition module
 	detected_places_or_interesting_objects = ["kitchen", "person", "ball", "sofa", "laptop", "restroom", "book", "mug", "diego milito", "television", "chair"]
 	
-
 	while not rospy.is_shutdown():
 		index = random.randint(0, len(detected_places_or_interesting_objects) - 1)
 		pub.publish(detected_places_or_interesting_objects[index])
 		rate.sleep()
 
+#main
 if __name__ == '__main__':
     try:
         test()
